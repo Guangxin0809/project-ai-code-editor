be65a40f58d2478addccda2e97c72c61f469c978
Yu19980809 <1572250276@qq.com>
2026-02-03 16:56:48 +0800

    feat: setup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+const Home = () => {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <p>Home</p>
+
+      <Button className="text-pink-500">Click</Button>
+    </div>
+  )
+}
+
+export default Home
